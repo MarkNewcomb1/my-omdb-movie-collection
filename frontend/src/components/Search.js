b11ceb900d235bec2e443bb538f446341634ac61
MarkNewcomb1 <wlmmn@hotmail.com>
2020-11-16 11:55:51 -0700

    some styling for input

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Search = ({searchValue, setSearchValue}) => {
     return (
         <div className="search-bar">
-            <label>Search </label>
+            <label>Search&nbsp;&nbsp;</label>
             <input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)}/>
         </div>
     )
